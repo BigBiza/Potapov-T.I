e0ed1239dd7c5b8d27b475eda194aacfb9994e70
BigBiza <113291530+BigBiza@users.noreply.github.com>
2022-10-25 19:53:44 +0300

    Delete дз 5 занятие directory

--- дз 5 занятие/2 номер.py
+++
@@ -1,8 +0,0 @@
-n=int(input('Введите число не меньшее 2:'))
-if n>=2:
-    for i in range(2,n+1):
-        if n%i==0:
-            print(i)
-            break
-else:
-    print('Введено неподходящее число')
--- дз 5 занятие/8 номер.py
+++
@@ -1,9 +0,0 @@
-a = int(input('Введите число:'))
-s = 0
-while a != 0:
-    b = a
-    a = int(input('Введите число:'))
-    if a == b:
-        s += 1
-else:
-    print(s)
