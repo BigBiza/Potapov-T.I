27a2f85ab0aaf808b6df9cb5884679a30ffe7e71
BigBiza <113291530+BigBiza@users.noreply.github.com>
2023-03-23 16:09:19 +0300

    Delete дз 1 занятие directory

--- дз 1 занятие/4.py
+++
@@ -1,8 +0,0 @@
-seconds = int(input('Введите количество секунд:'))
-minutes = seconds // 60
-hours = minutes // 60
-days = hours // 24
-secs = seconds % 60
-mins = minutes % 60
-h= hours % 24
-print(days,':',h,':',mins,':',secs,)
--- дз 1 занятие/7.py
+++
@@ -1,6 +0,0 @@
-0
-number=int(input('Введите число:'))
-if number % 2 == 0:
-    print('Число чётное')
-else:
-    print('Число нечётное')
