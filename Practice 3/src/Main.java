009c6ecb9d6194b9e66ae5ac8ff530f2971fbd68
BigBiza <113291530+BigBiza@users.noreply.github.com>
2023-12-08 23:57:04 +0300

    вот как-то так

--- Practice 3/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main implements java.io.Serializable {
-    public static void main(String[] args) {
-        CompareGroup compareGroup = new CompareGroup();
-        new Thread(() -> new MainStudentFrame(compareGroup).setVisible(true)).start();
-    }
-}
--- Practice 3/src/Student.java
+++
@@ -1,8 +0,0 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-
-public abstract class Student implements java.io.Serializable {
-    protected String name;
-    protected double group;
-}
